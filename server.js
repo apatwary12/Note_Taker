233727c2b1a7a9a66c84daf953184860abe7e296
Ari Patwary <apatwary12@yahoo.com>
2022-07-22 17:40:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const uuid = require('./helpers/uuid');
 const app = express();
 
 // Port
-const PORT = process.env.port || 3001;
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
